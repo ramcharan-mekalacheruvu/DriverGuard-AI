70b9a2f6ccb521f405c2aa22f6054ee130c6f16f
Ram_Charan <ramcharanmekalachervu@gmail.com>
2026-09-03 07:49:57 +0530

    Use VITE_API_URL env var instead of hardcoded localhost

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -4,7 +4,7 @@ import axios from "axios";
 
 const api = axios.create({
 
-    baseURL: "http://127.0.0.1:8000/api/",
+    baseURL: import.meta.env.VITE_API_URL || "http://127.0.0.1:8000/api/",
 
     headers: {
         "Content-Type": "application/json",
@@ -42,4 +42,3 @@ api.interceptors.request.use(
 
 
 export default api;
-
